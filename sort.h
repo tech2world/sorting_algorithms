90e09320bab31dc2c3eee5300bcc61619cdde18d
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-04-26 12:01:31 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -40,5 +40,9 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void quick_sort_helper(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
+void swap(int *a, int *b);
+
 
 #endif
