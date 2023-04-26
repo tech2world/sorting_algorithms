60a57fdf1f8aa68b39901e6a90ce0a3e4e40e0be
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-04-26 11:13:08 +0100

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,22 +12,23 @@
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, mid;
+    size_t i, j, min_index;
     int tmp;
 
     for (i=0; i < size - 1; i++)
     {
-        mid = i;
+        min_index = i;
         for (j = i + 1; j < size; j++)
         {
-            if (array[j] < array[mid])
-                mid = j;
+            if (array[j] < array[min_index])
+                min_index = j;
         }
-        if (mid != i)
+        if (min_index != i)
         {
             tmp = array[i];
-            array[i] = array[mid];
-            array[mid, size];
+            array[i] = array[min_index];
+            array[min_index] = tmp;
+            print_array(array, size);
         }
     }
 }
